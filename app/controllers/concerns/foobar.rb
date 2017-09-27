1628e226d63ff6b2394394b5c9f34185a35e017d
Ben Murphy <ben_murphy@berkeley.edu>
2017-09-27 01:13:58 -0700

    homework submission

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,12 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
   def initialize(baz)
+  	@baz = baz
+  end
+
+  def bar(var1, hash)
+  	thing = hash.values[0]
+  	var1 = var1.to_s
+  	"#{var1}#{@baz}#{thing}"
   end
 end
